c7b714e88f1b1780afd2a4ce21ee0bb72666070c
michalnand <michal.nand@gmail.com>
2024-10-06 19:12:46 +0200

    contrastive loss

--- xRLAgents/agents/loss_self_supervised.py
+++ xRLAgents/agents/loss_self_supervised.py
@@ -59,3 +59,48 @@ def loss_vicreg_func(za, zb):
     return loss, info
 
 
+def loss_contrastive_vicreg_func(za, zb):
+
+    # similarity loss
+    sim_loss = ((za - zb)**2).mean(dim=1)
+    sim_loss = sim_loss.mean()
+
+    # disimilarity loss
+    # random shuffle
+    idx = torch.randperm(zb.shape[0])
+    zb_perm = zb[idx]
+
+    dif_loss = torch.relu(1.0 - ((za - zb_perm)**2).mean(dim=1))
+    dif_loss = dif_loss.mean()
+
+
+    # variance loss
+    std_loss = loss_std_func(za)
+    std_loss+= loss_std_func(zb)
+   
+    # covariance loss 
+    cov_loss = loss_cov_func(za)
+    cov_loss+= loss_cov_func(zb)
+   
+    # total contrastive vicreg loss
+    loss = 1.0*sim_loss + 1.0*dif_loss + 1.0*std_loss + (1.0/25.0)*cov_loss
+
+    #info for log
+    z_mag     = ((za**2).mean()).detach().cpu().numpy().item()
+    z_mag_std = ((za**2).std()).detach().cpu().numpy().item()
+
+    sim_loss  = sim_loss.detach().cpu().numpy().item()
+    dif_loss  = dif_loss.detach().cpu().numpy().item()
+    std_loss  = std_loss.detach().cpu().numpy().item()
+    cov_loss  = cov_loss.detach().cpu().numpy().item()
+    
+    info = {}
+    info["mag_mean"] = z_mag
+    info["mag_std"]  = z_mag_std
+
+    info["sim"] = sim_loss
+    info["dif"] = dif_loss
+    info["std"] = std_loss
+    info["cov"] = cov_loss
+
+    return loss, info
