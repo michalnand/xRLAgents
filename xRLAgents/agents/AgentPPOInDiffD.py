68ce4fc7538668646a3db325564b575c0affea1e
michalnand <michal.nand@gmail.com>
2024-12-15 13:36:32 +0100

    goal based diffusion agent

--- xRLAgents/agents/AgentPPOInDiffD.py
+++ xRLAgents/agents/AgentPPOInDiffD.py
@@ -276,7 +276,7 @@ class AgentPPOInDiffD():
         #main IM training loop
         for batch_idx in range(batch_count):    
             #internal motivation loss, MSE diffusion    
-            states_now, states_next, _   = self.trajectory_buffer.sample_state_pairs(self.ss_batch_size, self.device)
+            states_now, states_next, _, _ = self.trajectory_buffer.sample_state_pairs(self.ss_batch_size, self.device)
             _, loss_diffusion  = self._internal_motivation(states_now, self.alpha_min, self.alpha_max, self.denoising_steps)
 
 
