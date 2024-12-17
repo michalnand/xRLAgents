165f36caec60baf79b32cdd0644d1dd5dd24bd45
michalnand <michal.nand@gmail.com>
2024-12-17 18:53:11 +0100

    goal based diff

--- xRLAgents/agents/GoalsBuffer.py
+++ xRLAgents/agents/GoalsBuffer.py
@@ -71,7 +71,7 @@ class GoalsBuffer():
         
         # check if need add new goal
         if d[closest_idx] > threshold and self.curr_ptr < self.states_raw.shape[0]:
-            self.states_raw[self.curr_ptr]       = state.copy()
+            self.states_raw[self.curr_ptr]       = state[0].copy()
             self.states_processed[self.curr_ptr] = state_processed.copy()
             self.scores[self.curr_ptr]           = score
             self.steps[self.curr_ptr]            = steps
