ecc60b8d9a9f208ba43d52444d13058565ced638
michalnand <michal.nand@gmail.com>
2024-12-22 17:10:50 +0100

    updated wrapper

--- xRLAgents/utils/WrapperMontezuma.py
+++ xRLAgents/utils/WrapperMontezuma.py
@@ -229,6 +229,20 @@ def WrapperMontezuma(env, height = 96, width = 96, frame_stacking = 4, max_steps
     env = ResizeEnv(env, height, width, frame_stacking)
     env = MaxSteps(env, max_steps)
     env = Rewards(env)
-    env = ExploredRoomsEnv(env)     
+    env = ExploredRoomsEnv(env, room_address = 3)     
+
+    return env
+
+
+
+def WrapperPitfall(env, height = 96, width = 96, frame_stacking = 4, max_steps = 4500):
+    #env = VideoRecorder(env)
+    env = NopOpsEnv(env)
+    env = StickyActionEnv(env)
+    env = RepeatActionEnv(env) 
+    env = ResizeEnv(env, height, width, frame_stacking)
+    env = MaxSteps(env, max_steps)
+    env = Rewards(env)
+    env = ExploredRoomsEnv(env, room_address = 1)     
 
     return env
--- xRLAgents/utils/__init__.py
+++ xRLAgents/utils/__init__.py
@@ -1,3 +1,3 @@
 from .WrapperAtari      import WrapperAtari
-from .WrapperMontezuma  import WrapperMontezuma
+from .WrapperMontezuma  import WrapperMontezuma, WrapperPitfall
 from .Augmentations     import Augmentations
