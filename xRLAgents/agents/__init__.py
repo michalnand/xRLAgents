5d4a4b80deeccf17a387cdd128b0ce7f210fe7da
michalnand <michal.nand@gmail.com>
2025-02-02 17:41:17 +0100

    aether mind - a contextual agent

--- xRLAgents/agents/__init__.py
+++ xRLAgents/agents/__init__.py
@@ -16,5 +16,6 @@ from .AgentPPOInDiffB       import *
 from .AgentPPOInDiffC       import *
 from .AgentPPOInDiffD       import *
 
+from .AgentAetherMind       import *
 
 from .loss_self_supervised  import *
