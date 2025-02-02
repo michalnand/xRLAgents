7ad036cd2be0ecd526c81312ddc8bfcb8634ad38
michalnand <michal.nand@gmail.com>
2025-02-02 18:18:29 +0100

    aether mind - a contextual agent

--- xRLAgents/agents/AgentAetherMind.py
+++ xRLAgents/agents/AgentAetherMind.py
@@ -158,8 +158,9 @@ class AgentAetherMind():
 
         for e in range(self.n_envs):
             state, _ = self.envs.reset(e)
+            state = numpy.expand_dims(state[:, 0], 1)
             print("state = ", state.shape)
-            self.state_mean+= numpy.expand_dims(state[:, 0], 1).copy()
+            self.state_mean+= state.copy()
 
         self.state_mean/= self.n_envs
         self.state_var = numpy.ones(self.state_shape,  dtype=numpy.float32)
