db20c684a37a2f9de6bd7b929fcd439a40e58e43
michalnand <michal.nand@gmail.com>
2025-02-03 08:10:02 +0100

    aether mind - a contextual agent

--- xRLAgents/agents/AgentAetherMind.py
+++ xRLAgents/agents/AgentAetherMind.py
@@ -265,8 +265,7 @@ class AgentAetherMind():
             if self.trajectory_buffer.is_full():
                 self.trajectory_buffer.compute_returns(self.gamma_ext, self.gamma_int)
                 
-                # TODO
-                #self.train()
+                self.train()
 
                 self.states_action_buffer.clear()
                 self.trajectory_buffer.clear()
