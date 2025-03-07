ed02e7afd2a7aacac772299d3010867f5b3cb4d6
michalnand <michal.nand@gmail.com>
2025-03-07 08:54:13 +0100

    skew loss

--- xRLAgents/agents/loss_self_supervised.py
+++ xRLAgents/agents/loss_self_supervised.py
@@ -26,13 +26,11 @@ def loss_cov_func(x):
 
 
 # skewness loss 
-def loss_skew_func(x, upper = 10.0):
+def loss_skew_func(x, upper = 1.0):
     mean    = x.mean()
     std     = x.std() + 1e-4 
     loss    = ((x - mean) / std)**3
     loss    = loss.mean()
-
-    print(loss)
 
     loss = torch.relu(upper - loss)
 
