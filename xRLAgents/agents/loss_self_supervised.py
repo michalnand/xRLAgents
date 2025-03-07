320320a71111d3d0145d1dd5568121ac6b778602
michalnand <michal.nand@gmail.com>
2025-03-07 09:14:41 +0100

    skew loss

--- xRLAgents/agents/loss_self_supervised.py
+++ xRLAgents/agents/loss_self_supervised.py
@@ -32,8 +32,6 @@ def loss_skew_func(x, target_skew = 0.98):
     loss    = ((x - mean) / std)**3
     loss    = loss.mean()
 
-    print("skew = ", mean, std, loss)
-
     loss = (target_skew - loss)**2
 
     return loss
