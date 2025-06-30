8f91052b010e97b8e6916b1a69603e693340ebc8
michalnand <michal.nand@gmail.com>
2025-06-30 20:08:40 +0200

    ppo gamma

--- xRLAgents/agents/__init__.py
+++ xRLAgents/agents/__init__.py
@@ -22,6 +22,5 @@ from .AgentPPOInDiffD       import *
 from .AgentAetherMindAlpha  import *
 from .AgentAetherMindBeta   import *
 from .AgentAetherMindGamma  import *
-from .AgentAetherMindDelta  import *
 
 from .loss_self_supervised  import *
