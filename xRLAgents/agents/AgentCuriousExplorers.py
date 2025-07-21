627298eecbfe18a1fe6ad2070a212cca780dfabf
michalnand <michal.nand@gmail.com>
2025-07-21 22:26:29 +0200

    ppo curious explorers

--- xRLAgents/agents/AgentCuriousExplorers.py
+++ xRLAgents/agents/AgentCuriousExplorers.py
@@ -194,11 +194,8 @@ class AgentCuriousExplorers():
         # rewards_int.shape = (n_explorers, batch_size)
         rewards_int        = rewards_int.float().detach().cpu().numpy()
 
-        print(rewards_int.shape, self.explorer_id, batch_indices)
         # select corresponding novelty  
         rewards_int_tmp = rewards_int[self.explorer_id, batch_indices]  
-
-        print("rewards_int ", rewards_int.shape, rewards_int_tmp.shape)
 
         rewards_int_scaled = numpy.clip(self.reward_int_coeff*rewards_int_tmp, 0.0, 1.0)
 
@@ -562,4 +559,3 @@ class AgentCuriousExplorers():
 
 
 
-    
