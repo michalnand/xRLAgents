03aa26ca7fa9e121e75fbff0493d4aeb90c694eb
michalnand <michal.nand@gmail.com>
2025-08-05 17:24:17 +0200

    im diff

--- xRLAgents/utils/WrapperMontezuma.py
+++ xRLAgents/utils/WrapperMontezuma.py
@@ -262,7 +262,7 @@ class ExploredRoomsEnv(gym.Wrapper):
 def WrapperMontezuma(env, height = 96, width = 96, frame_stacking = 4, max_steps = 4500):
     #env = VideoRecorder(env)
     env = RemoveTrunc(env)
-    #env = NopOpsEnv(env)
+    env = NopOpsEnv(env)
     #env = StickyActionEnv(env)
     #env = RepeatActionEnv(env) 
     #env = ResizeEnv(env, height, width, frame_stacking)
