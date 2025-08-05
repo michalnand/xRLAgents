0463ca95f2bda587d10743f000ed981aeb0be2f0
michalnand <michal.nand@gmail.com>
2025-08-05 17:24:29 +0200

    im diff

--- xRLAgents/utils/WrapperMontezuma.py
+++ xRLAgents/utils/WrapperMontezuma.py
@@ -263,7 +263,7 @@ def WrapperMontezuma(env, height = 96, width = 96, frame_stacking = 4, max_steps
     #env = VideoRecorder(env)
     env = RemoveTrunc(env)
     env = NopOpsEnv(env)
-    #env = StickyActionEnv(env)
+    env = StickyActionEnv(env)
     #env = RepeatActionEnv(env) 
     #env = ResizeEnv(env, height, width, frame_stacking)
     #env = MaxSteps(env, max_steps)
