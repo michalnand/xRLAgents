06baab1aaa8b3939d32ae2c97027e39b49bb7a30
michalnand <michal.nand@gmail.com>
2025-08-05 21:32:41 +0200

    im diff

--- xRLAgents/utils/WrapperMontezuma.py
+++ xRLAgents/utils/WrapperMontezuma.py
@@ -307,7 +307,10 @@ def WrapperMontezuma(env, height = 96, width = 96, frame_stacking = 4, max_steps
     
     env = MaxSteps(env, max_steps)
     env = Rewards(env)
-    env = ExploredRoomsEnv(env, room_address = 3)     
+    env = ExploredRoomsEnv(env, room_address = 3)   
+
+    print(env.unwrapped.ale.getFloat("repeat_action_probability"))  # Should be 0.25
+    print(env.unwrapped.ale.getInt("frame_skip"))                   # Should be 5  
 
     return env
 
