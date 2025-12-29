1ce1e7ef979e070893ad85f1f1a07f87db6fc0db
michalnand <michal.nand@gmail.com>
2025-12-29 16:56:26 +0100

    diversity diffusion explorer

--- xRLAgents/agents/AgentDiffExpF.py
+++ xRLAgents/agents/AgentDiffExpF.py
@@ -181,8 +181,6 @@ class AgentDiffExpF():
             if self.buffer_ptr < self.buffer_size:
                 p = 10*p
 
-            p = 1
-
             if numpy.random.rand() < self.buffer_prob:
                 self.states_buffer[self.buffer_ptr%self.buffer_size] = states_t[n].cpu().clone()
                 self.buffer_ptr = self.buffer_ptr + 1
