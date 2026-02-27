f86da6fb18fb62335d23999f62f74cf0018c2238
michalnand <michal.nand@gmail.com>
2026-02-27 19:41:22 +0100

    udpated causal IM

--- xRLAgents/agents/AgentDiffExpCausal.py
+++ xRLAgents/agents/AgentDiffExpCausal.py
@@ -502,11 +502,11 @@ class AgentDiffExpCausal():
         # this helps balance the dataset and stabilise training, 
         # as we have equal number of positive and negative samples
         dz_pos = z_now  - z_prev
-        #dz_neg = z_prev - z_now    
+        dz_neg = z_prev - z_now    
 
-        dz_pos = z_now  - z_prev
-        indices_shuffled = torch.randperm(z_prev.shape[0], device=self.device)
-        dz_neg = z_now  - z_prev[indices_shuffled]  
+        #dz_pos = z_now  - z_prev
+        #indices_shuffled = torch.randperm(z_prev.shape[0], device=self.device)
+        #dz_neg = z_now  - z_prev[indices_shuffled]  
 
         # causality novelty, model outputs sigmoid
         causality_pos = self.model.forward_im_causality(dz_pos)        
